63e6d3cf12a99b56a6f720e706e8a7da10a29bc2
Unknown <Stewart.Palmer@gmail.com>
2017-08-31 23:30:22 +0100

    Logo Update and Layout Expand
    New Logo, And Layout Expanded

--- OriginStoryFitness.Web/gulpfile.js
+++ OriginStoryFitness.Web/gulpfile.js
@@ -5,15 +5,22 @@ Click here to learn more. https://go.microsoft.com/fwlink/?LinkId=518007
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+
+
 gulp.task('default', function () {
     // place code for your default task here
 });
 
 
-
-
 gulp.task('styles', function () {
-    gulp.src('wwwroot/assets/scss/**/*.scss')
+    gulp.src('wwwroot/dev/scss/**/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./wwwroot/assets/css/'));
+        .pipe(gulp.dest('./wwwroot/dev/css/'));
+});
+
+gulp.task('minify-css', () => {
+    return gulp.src('./wwwroot/dev/css/main_stylesheet.css')
+        .pipe(cleanCSS({ compatibility: 'ie8' }))
+        .pipe(gulp.dest('./wwwroot/css/'));
 });
