252fdd871a7f517456ddf5f2396cfaae94f21d2d
Stewart Palmer <Stewart.Palmer@gmail.com>
2017-09-19 23:36:55 +0100

    SEO and CSS Animations

--- OriginStoryFitness.Web/wwwroot/js/site.js
+++ OriginStoryFitness.Web/wwwroot/js/site.js
@@ -1,1 +1,31 @@
 ﻿// Write your Javascript code.
+
+
+var animteHTML = function () {
+    var elements,
+        windowHeight
+    var init = function () {
+        elements = document.getElementsByClassName('featurelist')
+        windowHeight = window.innerHeight
+        _addEventHandlers() 
+
+    }
+
+    var _addEventHandlers = function () {
+        window.addEventListener('scroll', _checkPosition)
+        window.addEventListener('resize', init)
+    }
+
+    var _checkPosition = function () {
+        for (var i = 0; i < elements.length; i++) {
+            var posFromTop = elements[i].getBoundingClientRect().top
+            if (posFromTop - windowHeight <= 0) {
+                elements[i].className = elements[i].className.replace('element-hidden', 'slideInRight')
+            }
+        }
+    }
+    return {
+        init: init
+    }
+}
+animateHTML().init()
